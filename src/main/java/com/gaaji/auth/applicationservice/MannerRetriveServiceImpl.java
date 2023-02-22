d2c2cee4e1127582669327d7d7431d35fb499ee8
ansm6 <ansm6@host.docker.internal>
2023-02-22 17:00:52 +0900

    수정

--- src/main/java/com/gaaji/auth/applicationservice/MannerRetriveServiceImpl.java
+++ src/main/java/com/gaaji/auth/applicationservice/MannerRetriveServiceImpl.java
@@ -3,9 +3,18 @@ package com.gaaji.auth.applicationservice;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.gaaji.auth.domain.BadManner;
 import com.gaaji.auth.domain.GoodManner;
+import com.gaaji.auth.repository.AuthRepository;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
 public class MannerRetriveServiceImpl implements MannerRetriveService {
 
 	@Override
